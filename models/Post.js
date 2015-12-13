16357b92a999382b38d3e700f3b744fe50dbc8d3
PHI PHAM <phiph_57@vnu.edu.vn>
2015-12-14 02:04:55 +0700

    update proj

--- models/Post.js
+++ models/Post.js
@@ -2,33 +2,35 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 var Post = new keystone.List('Post', {
-	autokey: { from: 'name', path: 'key', unique: true },
-	drilldown: 'author'
+    autokey: {from: 'name', path: 'key', unique: true},
+    drilldown: 'author'
 });
 
 Post.add({
-	name: { type: String, required: true },
-	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	author: { type: Types.Relationship, ref: 'User', index: true },
-	publishedDate: { type: Types.Date, index: true },
-	image: { type: Types.CloudinaryImages },
-	price: {type: Number},
-	room : { type: Number},
-	area: { type: Number},
-	location: {type: String},
-	unit: {type: String},
-	content: {
-		brief: { type: Types.Html, wysiwyg: true, height: 150 },
-		extended: { type: Types.Html, wysiwyg: true, height: 400 },
-	},
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+    name: {type: String, required: true},
+    state: {type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true},
+    author: {type: Types.Relationship, ref: 'User', index: true},
+    publishedDate: {type: Types.Date, index: true},
+    image: {type: Types.CloudinaryImages},
+    price: {type: Number},
+    room: {type: Number},
+    area: {type: Number},
+    location: {type: String},
+    unit: {type: String},
+    lat: {type: Number},
+    long: {type: Number},
+    content: {
+        brief: {type: Types.Html, wysiwyg: true, height: 150},
+        extended: {type: Types.Html, wysiwyg: true, height: 400},
+    },
+    categories: {type: Types.Relationship, ref: 'PostCategory', many: true},
 });
 
 Post.schema.virtual('content.full').get(function () {
-	return this.content.extended || this.content.brief;
+    return this.content.extended || this.content.brief;
 });
 
-Post.relationship({ path: 'comments', ref: 'PostComment', refPath: 'post' });
+Post.relationship({path: 'comments', ref: 'PostComment', refPath: 'post'});
 
 Post.track = true;
 Post.defaultColumns = 'name, state|20%, author|20%, publishedDate|20%';
